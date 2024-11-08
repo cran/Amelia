2db05884466055126dafcd4879a6c7802c9bfcf3
Matthew Blackwell <mblackwell@gmail.com>
2024-11-08 00:50:02 +0000

    version 1.8.3

--- R/summary.mi.R
+++ R/summary.mi.R
@@ -1,3 +1,14 @@
+#' Summary of an mi object
+#' 
+#' Returns summary information about the list of multiply imputed data
+#' sets
+#'
+#' @param object an object of class \code{mi}. Typically, an output
+#'        from the function \code{amelia}.
+#' @param ... further arguments.
+#'
+#' @seealso \code{\link{amelia}}
+
 summary.mi <- function(object, ...) {
   m <- length(object)
   nv <- length(object[[1]])
